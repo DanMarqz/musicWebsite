55623c036849e46db4dd0ea1bcaedd144167e49a
DanMarqz <danielarturo.marquez@gmail.com>
2021-02-04 13:46:26 -0400

    commit

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
 	<head>
 		<title>Testing musicWebsite PHP</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	</head>
 
-	<body>
-		<?php phpinfo(); ?>	
+	<body class='bg-dark'>
+		<button type='button' class='btn btn-info'>Hola Mundo</button>
+		<hr class="text-danger">
+    <h1 class="text-light">Hola Mundo <a href='www.google.com' class="text-warning">Ingresa aquí, bebé.</a> </h1>	
+
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 	</body>
 <html>
